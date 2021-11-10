3553ca66d5a0bc18aef29a7480ec295f14abb6eb
francesco <francescosanti91@gmail.com>
2021-11-10 15:06:05 +0100

    bonus con immagine

---
+++ bonus/js/script.js
@@ -0,0 +1,11 @@
+var contenuti = new Vue(
+    {
+        el: "#elementi",
+        data: {
+            titolo: "Titolo principale",
+            classiTitolo: "titolo-principale",
+            percorsoImg: "https://unsplash.it/600/300?image=171",
+            classiImg: "immagine"
+        }
+    }
+);
